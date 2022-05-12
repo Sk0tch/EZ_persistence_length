2fa5f4f9f1105999b8957a40895212cde8708860
VasiliySubbotin-azur <88709733+VasiliySubbotin-azur@users.noreply.github.com>
2022-05-12 18:07:26 +0300

    2.3.1

--- src/pages/EDA.py
+++ src/pages/EDA.py
@@ -6,7 +6,10 @@ import pandas as pd
 import plotly.express as px
 from src.pages.Utils import LoadData, Parser, LinearMath, Painter, DataProcessor
 
-
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
 
 def EDA():
     st.header('Exploratory Data Analysis')
@@ -20,14 +23,35 @@ def EDA():
     with col1:
         st.write('coord data:')
         st.dataframe(data_coord.head(5))
+        csv_coord = convert_df(data_coord)
+        st.download_button(
+             label="Download data as CSV",
+             data=csv_coord,
+             file_name='coord_data.csv',
+             mime='text/csv',
+         )
     with col2:
         st.write('angles data:')
         st.dataframe(data_angles.head(5))
+        csv_angles = convert_df(data_angles)
+        st.download_button(
+             label="Download data as CSV",
+             data=csv_angles,
+             file_name='data_angles.csv',
+             mime='text/csv',
+         )
     with col3:
         st.write('grouping data:')
         st.dataframe(group.head(5))
+        csv_group = convert_df(group)
+        st.download_button(
+             label="Download data as CSV",
+             data=csv_group,
+             file_name='group_data.csv',
+             mime='text/csv',
+         )
 
 
     # st.dataframe(data.head(5))
-    chains_numb = st.number_input(label='chians for view', min_value=1, max_value=max_chains, step=1)
+    chains_numb = st.number_input(label='number of chains to display', min_value=1, max_value=max_chains, step=1)
     st.plotly_chart(Painter.plot_chains(data_coord, chains_numb))
